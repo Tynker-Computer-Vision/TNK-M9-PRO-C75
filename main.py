33df93da7e33bab01da8481064043cb372178116
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-06-14 13:54:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from keras.models import load_model
+
+from cvzone.FaceDetectionModule import FaceDetector
+import cv2
+import numpy as np
+cap = cv2.VideoCapture(0)
+detector = FaceDetector()
+
+ageDetectionModel = load_model('age_model_50epochs.h5', compile=False)
+
+while True:
+    try:
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        img, bboxs = detector.findFaces(img, draw=False)
+
+        if bboxs:
+            for box in bboxs:
+                X = box['bbox'][0]
+                Y = box['bbox'][1]
+                W = box['bbox'][2]
+                H = box['bbox'][3]
+        
+                croppedImg = img[Y:Y+H, X:X+W]
+                resizedImg = [cv2.resize(croppedImg, (200, 200))]
+                resizedImg = np.array(resizedImg)
+                prediction = ageDetectionModel.predict(resizedImg)
+
+                img = cv2.putText(img, str(int(prediction[0][0])), (X, Y), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 2)
+                img = cv2.rectangle(img, (X, Y), (X+W, Y+H), (255, 255, 255), 1)
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    except Exception as e:
+        print("Exception", e)
+cap.release()
+cv2.destroyAllWindows()
